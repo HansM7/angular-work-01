219b87211bf02fb96220689cbd64bf8faaf5f38d
HansM7 <melchorhans5@gmail.com>
2023-09-15 16:59:18 -0500

    001

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
+import { ContentWrapperComponent } from './content-wrapper/content-wrapper.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CanvaSidebarComponent } from './canva-sidebar/canva-sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ContentComponent,
+    ContentWrapperComponent,
+    ToolbarComponent,
+    CanvaSidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/canva-sidebar/canva-sidebar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-canva-sidebar',
+  templateUrl: './canva-sidebar.component.html',
+  styleUrls: ['./canva-sidebar.component.scss']
+})
+export class CanvaSidebarComponent {
+
+}
